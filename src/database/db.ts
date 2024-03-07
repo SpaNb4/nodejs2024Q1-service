cb13d02fbb44b6d79d67674aa11d8b89796b3570
Dzmitry Yarmoshkin <spanb4@gmail.com>
2024-03-07 21:39:09 +0300

    clear db

--- src/database/db.ts
+++ src/database/db.ts
@@ -15,58 +15,10 @@ interface Database {
 }
 
 export const db: Database = {
-  users: [
-    {
-      id: '84ee5f9a-f00f-43ee-897d-e80892a83492',
-      login: 'John',
-      password: 'password',
-      version: 1,
-      createdAt: Date.now(),
-      updatedAt: Date.now(),
-    },
-    {
-      id: '079080c7-a79f-41b8-b0c7-a8106d740e03',
-      login: 'Jane',
-      password: 'password',
-      version: 1,
-      createdAt: Date.now(),
-      updatedAt: Date.now(),
-    },
-    {
-      id: 'b7f43170-1ce7-479e-9632-a5881991ef63',
-      login: 'Mike',
-      password: 'password',
-      version: 1,
-      createdAt: Date.now(),
-      updatedAt: Date.now(),
-    },
-    {
-      id: '407a6857-5d8b-4114-abf7-16c3cf926e72',
-      login: 'Emily',
-      password: 'password',
-      version: 1,
-      createdAt: Date.now(),
-      updatedAt: Date.now(),
-    },
-    {
-      id: 'b2939ec7-2fc2-4a6e-9ff0-f37cded8fe89',
-      login: 'David',
-      password: 'password',
-      version: 1,
-      createdAt: Date.now(),
-      updatedAt: Date.now(),
-    },
-  ],
+  users: [],
   artists: [],
   tracks: [],
-  albums: [
-    {
-      id: 'f4b3b3e3-3b6e-4f3d-8f6e-6e3d4f3b3b4f',
-      name: 'The Dark Side of the Moon',
-      year: 1973,
-      artistId: null,
-    },
-  ],
+  albums: [],
   favorites: {
     artists: [],
     albums: [],
