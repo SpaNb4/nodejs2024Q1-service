fc7879d575ea65d9d898eb47e863c791f08a9f98
Dzmitry Yarmoshkin <spanb4@gmail.com>
2024-03-30 15:46:03 +0300

    log response

--- src/interceptors/http-interceptor.ts
+++ src/interceptors/http-interceptor.ts
@@ -8,19 +8,45 @@ import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
 import { Logger } from 'src/logger/logger.service';
 
+interface DataToLog {
+  fieldName: string;
+  fieldValue: string;
+}
+
+const formatData = (dataToLog: DataToLog[]) => {
+  return dataToLog
+    .map(
+      ({ fieldName, fieldValue }) =>
+        `${fieldName}: ${JSON.stringify(fieldValue, null, 2)}`,
+    )
+    .join('\n');
+};
+
 @Injectable()
 export class HttpInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     const httpContext = context.switchToHttp();
     const request = httpContext.getRequest();
+    const response = httpContext.getResponse();
     const { url, body, query } = request;
+    const { statusCode } = response;
 
     const logger = new Logger();
 
     return next.handle().pipe(
-      tap(() => {
+      tap((data) => {
         logger.log(
-          `${url} body=${JSON.stringify(body)} query=${JSON.stringify(query)}`,
+          `REQUEST: ${formatData([
+            { fieldName: 'url', fieldValue: url },
+            { fieldName: 'body', fieldValue: body },
+            { fieldName: 'query', fieldValue: query },
+          ])}`,
+        );
+        logger.log(
+          `RESPONSE: ${formatData([
+            { fieldName: 'statusCode', fieldValue: statusCode },
+            { fieldName: 'data', fieldValue: data },
+          ])}`,
         );
       }),
     );
